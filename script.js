cb3902f50def9c69c8e359862f80ce4e7c2a0531
abdul-quadri <abdulquadri7@gmail.com>
2023-07-08 09:38:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
